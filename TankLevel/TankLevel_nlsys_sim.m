ef28d418f12dbd26afccd4a58bed8ff4925fcc6f
jonaswagner2826 <47377464+jonaswagner2826@users.noreply.github.com>
2020-12-16 01:12:08 -0600

    added nlsys to project (I guess)

--- TankLevel/TankLevel_nlsys_sim.m
+++ TankLevel/TankLevel_nlsys_sim.m
@@ -18,3 +18,8 @@ sys_plant = nlsys(f,h,x_0)
 u_0 = [0.25;0.5];
 sys_plant_1 = sys_plant.update(u_0)
 
+sys_cntrl = nlsys(pid(1,1));
+
+sys_cntrl = nlsys.append(sys_cntrl,sys_cntrl)
+
+% sys_plant_cntrl = nlsys.series(sys_plant,sys_cntrl)
