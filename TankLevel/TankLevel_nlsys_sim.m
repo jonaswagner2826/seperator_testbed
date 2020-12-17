86f900b1dd6f29b8096f1611a65870aedcf9f46f
jonaswagner2826 <47377464+jonaswagner2826@users.noreply.github.com>
2020-12-16 21:54:49 -0600

    Added nlsim.plot() ... this has plots of states and I/O

--- TankLevel/TankLevel_nlsys_sim.m
+++ TankLevel/TankLevel_nlsys_sim.m
@@ -31,16 +31,34 @@ N = 100;
 t_step = 0.1;
 t_max = N * t_step - t_step;
 T = reshape(0:t_step:t_max,N,1);
-u_0= [0.5, 0.25];
-U = u_0.* abs(sin(T));
+u_0= [0.5, 0.25]; %U'
+U = u_0.* abs(sin(T)); %U'
+SYS_plant = nlsim(sys_plant,U,T,x_0);
+X_plant = SYS_plant.X;
+T_plant = SYS_plant.T;
+U_plant = SYS_plant.U;
 
 
+
+
+
+% SS = SYS.ssModel This does not work... issue with substitution...
 
 % subplot(3,1,1)
-figure()
-SYS = sys_plant.nlsim(sys_plant,U,T,x_0);
-X = cell2mat({SYS(1:N).x});
-plot(X')
+% figure()
+
+
+% plot(T_plant',X_plant')
+
+% fig = figure();
+%Option 1:
+[fig,~] = SYS_plant.plot(1,1,1,-1,2,1);
+[fig,~] = SYS_plant.plot(2,2,2,fig,2,2);
+
+%Option 2:
+SYS_plant.plot;
+
+
 % 
 % subplot(3,1,2)
 % SYS = sys_plant_cntrl_open.nlsim(sys_plant_cntrl_open,U,T);
