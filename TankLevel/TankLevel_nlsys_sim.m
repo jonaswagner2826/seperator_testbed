097a99f241a7444526a91a208c3442d8cf2346e3
jonaswagner2826 <47377464+jonaswagner2826@users.noreply.github.com>
2020-12-18 17:45:33 -0600

    got feedback system simulation and plotting working

--- TankLevel/TankLevel_nlsys_sim.m
+++ TankLevel/TankLevel_nlsys_sim.m
@@ -4,7 +4,7 @@
 clear
 close all
 
-% Initial Setup
+%% Initial Setup
 
 % Nonlinear State Eq
 f = @TankLevelDynamics_SimplifiedNonlinear;
@@ -18,13 +18,13 @@ sys_plant = nlsys(f,h,x_0)
 u_0 = [0.25;0.5];
 sys_plant_1 = sys_plant.update(u_0);
 
-% Simulation ---------------------
+%% Simulation ---------------------
 N = 100;
 t_step = 0.1;
 t_max = N * t_step - t_step;
-T = reshape(0:t_step:t_max,N,1);
-u_0= [0.5, 0.25]; %U'
-U = u_0.* abs(sin(T)); %U'
+T = 0:t_step:t_max;
+u_0= [1.0; 0.5];
+U = [u_0(1) * ones(size(T)); u_0(2) * abs(sin(T))];
 
 %Plant
 SYS_plant = nlsim(sys_plant,U,T,x_0);
@@ -32,25 +32,31 @@ X_plant = SYS_plant.X;
 T_plant = SYS_plant.T;
 U_plant = SYS_plant.U;
 
-% ploting
-%Option 1:
+%% ploting
+%Option 1: Side by side (with 1 and 2 representing which states)
 [fig,~] = SYS_plant.plot(1,1,1,-1,2,1);
 [fig,~] = SYS_plant.plot(2,2,2,fig,2,2);
 
-%Option 2:
-SYS_plant.plot;
+%Option 2: Comparrison (default)
+% SYS_plant.plot;
 
-close all
-%PID control
-k_p = 1;
-k_i = 0.1;
-k_d = 0.001;
+%Option 3: No output (w/ zero)
+% SYS_plant.plot(-1,-1,0);
+
+% close all
+
+%% PID control
+k_p = 1.5;
+k_i = 1;
+k_d = 0.1;
 
 sys_cntrl = nlsys.pid(k_p,k_i,k_d);
 sys_cntrl = nlsys.append(sys_cntrl,sys_cntrl); %U is two dims...
-sys_plant_cntrl_open = nlsys.series(sys_plant,sys_cntrl); %Foward loop
+sys_plant_cntrl_open = nlsys.series(sys_cntrl, sys_plant); %Foward loop
 sys_plant_cntrl_closed = nlfeedback(sys_plant_cntrl_open); %Unity feedback
 
-SYS_closed = nlsim(sys_plant_cntrl_closed,U,T)
+U = [x_0(1)*ones(size(T));x_0(2)*ones(size(T))+sin(T)];
 
+SYS_closed = nlsim(sys_plant_cntrl_closed,U,T);
 
+SYS_closed.plot([7,8],[1,2],-1);
